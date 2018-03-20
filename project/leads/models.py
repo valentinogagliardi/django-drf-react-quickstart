5a9b8c00105cc7c7af74c0c1309a261840f63a33
Valentino Gagliardi <valentino@valentinog.com>
2018-03-20 12:47:26 +0100

    Added: example for testing __str__ on a model

--- project/leads/models.py
+++ project/leads/models.py
@@ -5,3 +5,6 @@ class Lead(models.Model):
     email = models.EmailField()
     message = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
--- project/leads/tests.py
+++ project/leads/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Lead
 
-# Create your tests here.
+class LeadModelTest(TestCase):
+    def test_str_representation(self):
+        lead = Lead.objects.create(
+            name='Tom',
+            email='tom@whatever.com',
+            message='I need some help with a project'
+        )
+        self.assertEqual(str(lead), 'tom@whatever.com')
+
