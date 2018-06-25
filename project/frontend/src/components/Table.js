ac84fdebc651c365086b5c083e2b0ad9becd25c4
Valentino Gagliardi <valentino@valentinog.com>
2018-06-25 15:53:11 +0200

    Use weak-key in place of shortid

--- project/frontend/src/components/Table.js
+++ project/frontend/src/components/Table.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import shortid from "shortid";
-
-const uuid = shortid.generate;
+import key from "weak-key";
 
 const Table = ({ data }) =>
   !data.length ? null : (
@@ -13,13 +11,13 @@ const Table = ({ data }) =>
       <table className="table is-striped">
         <thead>
           <tr>
-            {Object.entries(data[0]).map(el => <th key={uuid()}>{el[0]}</th>)}
+            {Object.entries(data[0]).map(el => <th key={key(el)}>{el[0]}</th>)}
           </tr>
         </thead>
         <tbody>
           {data.map(el => (
             <tr key={el.id}>
-              {Object.entries(el).map(el => <td key={uuid()}>{el[1]}</td>)}
+              {Object.entries(el).map(el => <td key={key(el)}>{el[1]}</td>)}
             </tr>
           ))}
         </tbody>
